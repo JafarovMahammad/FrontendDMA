db3d93cc729e26d8596cb3e2abf690cae3e8d216
JafarovMahammad <maqaccc51@gmail.com>
2024-04-01 12:29:41 +0400

    java added file

---
+++ java/main.js
@@ -0,0 +1,158 @@
+// function proccess(){
+// document.getElementById('change').innerHTML='Hello I m new topic.'
+// document.getElementById('change').style.color='black'
+// }
+
+// function preview(){
+// 	document.getElementById('change').innerHTML='Click on the button and change text';
+// 	document.getElementById('change').style.color='red'
+// }
+
+// task2
+// const name = "Mehemmed"
+// console.log(name)
+
+// task3
+// person
+
+// task4
+// let bookPrice = 750;
+// let bookAmount = 14;
+// let total = bookPrice * bookAmount
+// console.log(total);
+// let num = 45;
+// console.log(num ** 5);
+// console.log(num % 3);
+// task5
+// let kmhSpeed = 75;
+// let count = kmhSpeed / 3600 * 100
+// console.log(count);
+
+// task6
+// let thisyear = 2024
+// let birthday = prompt('Dogum tarixini yaz')
+// let totalday = 365 * (thisyear - birthday)
+
+// console.log(totalday);
+
+// task8
+
+// let firstname = "Muhammed"
+// let lastname = "Jafarli"
+// let age = 28
+// console.log("Sizin Adiniz " + [firstname] + " " + [lastname] + " " + [age] + " " + "Tanisligimiza cox sadam" ) ;
+
+// let fullname = "Mahammad Jafarli"
+// console.log(fullname.length);
+
+
+// let firstname = "Mahammad"
+// let surname = "Jafarli"
+// console.log('Sizi bir daha görməyə şadıq' + " " + [firstname] + " " + [surname]);
+// console.log(`Sizi bir daha görməyə şadıq ${firstname} ${surname}`);
+
+// let = prompt('Adınızı və Soyadınızı daxil edin')
+// let userName = "SOYAD"
+// let surname = "AD"
+// let fullname = (prompt + [surname] + [userName]  );
+// console.log(fullname)
+
+// b=0
+// c=-3
+// d=0
+
+// task12
+
+// let firstName = prompt (" Adinizi qeyd edin")
+// let surName = prompt ("Soyadinizi qeyd edin")
+// console.log(` ${surName} ${firstName}`);
+// task13
+// let message = prompt ('Adinizi yazin')
+// let name1 = "Salam"
+// let name2 = "Ad"
+// console.log(`${name1} ${name2}`);
+
+// task14
+
+// const a= + prompt("first number")
+// const b= + prompt("second number")
+// console.log(a-b);
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+
+// let a = + prompt('qiymeti yazin')
+// let a1 = + prompt('qepiyi yazin')
+// let b = "Manat"
+// let c = "qepik"
+
+// console.log(`${a} ${b} ${a1} ${c}`);
+// let number = prompt("qiymeti daxil edin")
+// let priceList = number.split(".")
+// console.log(`${priceList[0]} "Manat" ${priceList[1]} "Qepik"`)
+// let userName = prompt("Ad,soyad,Ata adı daxil edin");
+// let fullName = userName.split(" ");
+// let user = fullName[0];
+// let surname = fullName[1][0];
+// let parent = fullName[2][0];
+// console.log(`$(user) $(surname).$(parent).`);
+
+// let depozit = prompt("depoziti daxil edin")
+// let faiz = prompt("faizi daxil edin")
+// let date = prompt ("ayi daxil edin")
+// let menfeet = (depozit * faiz)/100
+// let qazanc = date * menfeet
+// console.log(qazanc);
+
+// let num = +prompt("eded daxil edin");
+
+// for (num > 0) {
+//     let start = num
+//     let count = 0
+//     for (start > 0) {
+//         if (num % start === 0) {
+//             count++;
+//         }
+//         start--;
+//     }
+//     if (count === 2) {
+//     console.log("sade eddedir", num);
+//     }
+// }
+
+// function calc(a,b,c) {
+//     switch (c) {
+//         case "+":
+//             console.log(a+b);
+//             break;
+//     case "-";
+//     console.log(a-b);
+//     break;
+//     case "*":
+//         console.log(a*b);
+//         break;
+//         case "/":
+//             console.log(a/b);
+//             break;
+
+//         default:
+//             console.log(yanlis deyer);
+//     }
+// }
+
+// let ededler = [
+//     33, 25, 87, 95, 36, 28, 47, 49,50]
+    
+// let count=0;
+// let count1=0
+// for(i=0; i<ededler.length; ++i) {
+
+//     if(ededler[i]%2==0)
+//    count+=ededler[i];
+
+//    else{
+//     count1+=ededler[i]
+//    }
+// }
+
+
