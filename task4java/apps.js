e13ba03526d9aa3ccb77dd0269e4f2b718f0d96d
JafarovMahammad <maqaccc51@gmail.com>
2024-04-03 13:23:56 +0400

    task4java

---
+++ task4java/apps.js
@@ -0,0 +1,65 @@
+
+// massiv = ["Emin", "Aynur", "Orxan"]
+// massiv[1] = "Classified"
+// console.log(massiv);
+
+// let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jedi'];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(`Episode ${i + 4}: ${arr[i]}`);
+// }
+
+// let salary = [
+//     [60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60],
+//     [75, 75, 75, 75, 75, 75, 70, 77, 75, 75, 70, 75],
+//     [150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 180],
+//     [65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65],
+//     [80, 80, 80, 80, 80, 80, 80, 105, 105, 105, 105, 105],
+//     [65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65],
+//     [80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 120],
+//     [65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65],
+//     [80, 80, 80, 80, 80, 80, 80, 90, 90, 90, 90, 90],
+//     [75, 75, 75, 75, 75, 75, 75, 75, 75, 75, 70, 75]
+// ];
+
+// function getAnnualSalary() {
+//     let annualSalary = 0;
+//     for (let i = 0; i < salary.length; i++) {
+//         for (let j = 0; j < salary[i].length; j++) {
+//             annualSalary += salary[i][j];
+//         }
+//     }
+//     return annualSalary;
+// }
+
+
+// function getMonthlySalary(monthNumber) {
+//     let monthlySalary = [];
+//     for (let i = 0; i < salary.length; i++) {
+//         monthlySalary.push(salary[i][monthNumber - 1]);
+//     }
+//     return monthlySalary;
+// }
+
+// function getQuarterSalary(quarterNumber) {
+//     let quarterSalary = 0;
+//     for (let i = (quarterNumber - 1) * 3; i < quarterNumber * 3; i++) {
+//         for (let j = 0; j < salary[i].length; j++) {
+//             quarterSalary += salary[i][j];
+//         }
+//     }
+//     return quarterSalary;
+// }
+
+// console.log("Ümumi əmək haqqı ilə:", getAnnualSalary());
+// console.log("3-cü ayın maaşları:", getMonthlySalary(3));
+// console.log("2-ci rübün əmək haqqı ilə:", getQuarterSalary(2));
+
+// let text = '32, 31, 34, 36, 31';
+
+// let newText = text.split(',').join(';');
+
+// console.log(newText);
+
+
+
